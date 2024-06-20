c152eaa87dd96798901c85190601a162884da55b
temor01 <134977930+temor01@users.noreply.github.com>
2024-06-19 23:39:19 -0600

    getComputerChoice function successfully created

--- javascript.js
+++ javascript.js
@@ -6,3 +6,11 @@ console.log("Hello World");
 // Storing game choices in an array
 var answers = ["Rock", "Paper", "Scissors"];
 
+// Now we're going to make a program that asks our computer to randomly choose one of the answers
+// We will start by creating a function called 'getComputerChoice', and run a quick test on it
+function getComputerChoice() {
+    choice = answers[(Math.floor(Math.random() * answers.length))];
+    console.log(choice);
+}
+
+getComputerChoice();
